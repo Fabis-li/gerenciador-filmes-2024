beb35d68a34ecaaa16e476a0cba216bc004b0466
Fabis-li <fabiano_slima@hotmail.com>
2024-10-03 21:46:28 -0300

    criado busca de filme

---
+++ src/app/components/busca/busca.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FilmeService } from '../../services/filme.service';
+import { ResultadoBuscaFilme } from '../../models/resultado-busca-filme.model';
+import { ListagemFilme } from '../../models/listagem-filme.model';
+import { formatDate } from '@angular/common';
+
+@Component({
+  selector: 'app-busca',
+  standalone: true,
+  imports: [],
+  templateUrl: './busca.component.html',
+  styleUrl: './busca.component.scss'
+})
+export class BuscaComponent implements OnInit {
+  public resultadoBusca?: ResultadoBuscaFilme;
+
+
+  constructor(private filmeService: FilmeService) { }
+  ngOnInit(): void {
+    this.buscar('batman');
+  }
+
+  public buscar(query:string): void {
+    if(query.length < 1) return;
+
+    this.filmeService.buscarFilmes(query).subscribe((res) => {
+      this.resultadoBusca = this.mapearResultadoBusca(res);
+
+      console.log(this.resultadoBusca);
+    });
+  }
+
+  private mapearResultadoBusca(obj: any): ResultadoBuscaFilme {
+    return {
+      pagina: obj.page,
+      quantidadePaginas: obj.total_pages,
+      quantidadeResultados: obj.total_results,
+      filmes: obj.results.map(this.mapearListagemFilme)
+    }
+  }
+
+  private mapearListagemFilme(obj: any): ListagemFilme {
+    return {
+      id: obj.id,
+      titulo: obj.title,
+      lancamento: formatDate(obj.release_date, 'medium', 'pt-BR'),
+      urlImagem: 'https://image.tmdb.org/t/p/w300/' + obj.poster_path,
+      porcentagemNota: (obj.vote_average * 10).toFixed(0),
+    }
+  }
+}
